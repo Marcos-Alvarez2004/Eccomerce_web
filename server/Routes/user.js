b5b71b1eaf6c58bc5c3912489bb4406f43b29434
Marcos Alvarez <137237087+Marcos-Alvarez2004@users.noreply.github.com>
2024-04-30 19:19:34 -0300

    Ruta de usuario para registro

--- server/Routes/user.js
+++ server/Routes/user.js
@@ -1,2 +1,31 @@
+// VARIABLES
+import User from "./User.js";
 import express from "express";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+// ROUTER
 const router = express.Router();
+// REGISTER
+router.post("/register", async (req, res) => {
+  try {
+    // TOMAR INFORMACION
+    const { name, email, password } = req.body;
+    // CREAR USUARIO
+    const newUser = new User({
+      name,
+      email,
+      password,
+    });
+    // GUARDAR EL USUARIO
+    await newUser.save();
+    // ENVIAR MENSAJE DE USUARIO REGISTRADO
+    res
+      .status(200)
+      .json({ message: "Usuario registrado con exito!", user: newUser });
+  } catch (err) {
+    res.status(500).json({
+      message: "Algo salio mal!",
+      error: err.message,
+    });
+  }
+});
