3fcd7ae07a957bdf7f40f4b0b3f8c34b19774600
Marcos Alvarez <137237087+Marcos-Alvarez2004@users.noreply.github.com>
2024-05-02 14:30:30 -0300

    El navbar fue agregado al home y en home de la carpeta Pages fue modificado

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,34 +1,17 @@
 // REACT
 import React from "react";
-// DEPENDENCIAS
-import { useSelector, useDispatch } from "react-redux";
+
 // ARCHIVOS
-import { setLogout } from "../redux/state";
-import { Link } from "react-router-dom";
 import Navbar from "../components/Navbar";
 
 const Home = () => {
-  const user = useSelector((state) => state.user);
-  const dispatch = useDispatch();
   return (
-    <section
-      className={`h-screen bg-cover bg-center bg-no-repeat bg-[url("/fondoHome.jpg")]`}
-    >
-      <Navbar />
-      <div className="flex flex-col justify-center items-center gap-4">
-        <h1 className="flex gap-2">
-          Hola <p className="text-red-500">{user.name}</p>!
-        </h1>
-        <Link
-          className="bg-indigo-500 p-2 rounded-md"
-          to={"/login"}
-          onClick={() => {
-            dispatch(setLogout());
-          }}
-        >
-          Log Out
-        </Link>
-      </div>
+    <section>
+      <main
+        className={`h-screen bg-cover bg-center bg-no-repeat bg-[url("/fondoHome.jpg")]`}
+      >
+        <Navbar />
+      </main>
     </section>
   );
 };
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,58 @@
+// REACT
 import React from "react";
-
+// DEPENDENCIAS
+import { Link } from "react-router-dom";
+import { PiBag } from "react-icons/pi";
+import { useSelector, useDispatch } from "react-redux";
+// ARCHIVOS
+import { setLogout } from "../redux/state";
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const user = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+  return (
+    <header className="fixed w-full flex justify-between items-center p-8 bg-black/80 border-b border-white">
+      {/* LOGO */}
+      <div>
+        <h1 className="text-4xl">Logo</h1>
+      </div>
+      {/* MENU */}
+      <nav className="flex gap-x-8 menu">
+        <Link className="link">
+          <span className="text-xl font-normal">Home</span>
+        </Link>
+        <Link className="link">
+          <span className="text-xl font-normal">Acerca de</span>
+        </Link>
+        <Link className="link">
+          <span className="text-xl font-normal">Productos</span>
+        </Link>
+        <Link className="link">
+          <span className="text-xl font-normal">Contacto</span>
+        </Link>
+      </nav>
+
+      {/* USER */}
+      <section className="flex gap-x-4">
+        <div className="text-4xl">
+          <PiBag />
+        </div>
+        <div>
+          Hola usuario <b className="text-red-500">{user.name}</b> !
+        </div>
+        <div>
+          <Link
+            className="bg-indigo-500 p-2 rounded-md"
+            to={"/login"}
+            onClick={() => {
+              dispatch(setLogout());
+            }}
+          >
+            Log Out
+          </Link>
+        </div>
+      </section>
+    </header>
+  );
 };
 
 export default Navbar;
