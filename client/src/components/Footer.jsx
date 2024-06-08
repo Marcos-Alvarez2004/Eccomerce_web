86fa5f835a6d8832c12d59273bbe71999b10e3eb
Marcos Alvarez <137237087+Marcos-Alvarez2004@users.noreply.github.com>
2024-06-08 16:27:50 -0300

    Se agrego link en footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -7,22 +7,42 @@ const Footer = () => {
       <div className="container mx-auto flex justify-center items-center flex-col gap-y-8">
         <ul className="flex space-x-8 text-3xl">
           <li>
-            <a href="#" className="text-white hover:text-gray-400">
+            <a
+              href="https://www.facebook.com/profile.php?id=100076853130871&mibextid=ZbWKwL"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-white hover:text-gray-400"
+            >
               <FaFacebook />
             </a>
           </li>
           <li>
-            <a href="#" className="text-white hover:text-gray-400">
+            <a
+              href="https://x.com/markuspaaaaa"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-white hover:text-gray-400"
+            >
               <FaTwitter />
             </a>
           </li>
           <li>
-            <a href="#" className="text-white hover:text-gray-400">
+            <a
+              href="https://www.instagram.com/marcosalv12?igsh=MW5pdXYyMTF6a3Vibw=="
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-white hover:text-gray-400"
+            >
               <FaInstagram />
             </a>
           </li>
           <li>
-            <a href="#" className="text-white hover:text-gray-400">
+            <a
+              href="https://www.linkedin.com/in/marcosalvarezmedero?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-white hover:text-gray-400"
+            >
               <FaLinkedin />
             </a>
           </li>
